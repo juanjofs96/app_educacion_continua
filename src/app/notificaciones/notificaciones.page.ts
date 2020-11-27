5161a8c0bd1bb514d11487bae8329efc121450ec
kavelino <kleberth@icloud.com>
2020-11-26 21:31:48 -0500

    [#173926979] Página de contacto

--- src/app/notificaciones/notificaciones.page.ts
+++ src/app/notificaciones/notificaciones.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AppComponent } from '../app.component';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-notificaciones',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificacionesPage implements OnInit {
 
-  constructor() { }
+  private listNotificaciones = []
+  private find:boolean;
+  constructor(private App: AppComponent, private alertController: AlertController,private router:Router) { }
 
   ngOnInit() {
   }
 
+
+
+  
+  ionViewDidEnter() {
+    this.listNotificaciones = []
+    this.find=false;
+    if(this.App.id_User!=null) {
+      this.getCursos();
+    }
+  }
+  async getCursos() {
+    var self = this;
+    await $.getJSON("https://prueba-63695.firebaseio.com/cursos.json", function (data_cursos) {
+      $.getJSON("https://prueba-63695.firebaseio.com/usuarios.json", function (data_users) {
+        for (let i = 0; i < data_users.length; i++) {
+            for (let j = 0; j < data_cursos.length; j++) {
+              if (data_users[i].id_user == data_cursos[j].id_user && data_users[i].id_user == self.App.id_User) {
+                self.listNotificaciones.push(data_cursos[j]);
+                self.find=true;
+              }
+            }
+        }
+        if(!self.find){
+          self.alertCurso();
+        }
+      })
+    })
+    
+  }
+
+  async alertCurso() {
+      const alert = await this.alertController.create({
+        header: 'Mensaje',
+        subHeader: 'Sin cursos registrados',
+        message: 'Usted no pertenece a ningún curso',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+    }
 }
