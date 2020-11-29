9674251f0c6e5f75ac392d4f2b2824e2d010ba6b
kavelino <kleberth@icloud.com>
2020-11-29 15:21:56 -0500

    [#173926800] Listado de notificaciones por participante

--- src/app/notificaciones/notificaciones.page.ts
+++ src/app/notificaciones/notificaciones.page.ts
@@ -12,48 +12,43 @@ import * as $ from "jquery";
 export class NotificacionesPage implements OnInit {
 
   private listNotificaciones = []
-  private find:boolean;
-  constructor(private App: AppComponent, private alertController: AlertController,private router:Router) { }
+  constructor(private App: AppComponent, private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
 
-
-
-  
   ionViewDidEnter() {
     this.listNotificaciones = []
-    this.find=false;
-    if(this.App.id_User!=null) {
+    if (this.App.id_User != null) {
       this.getNotificaciones();
     }
   }
   async getNotificaciones() {
     var self = this;
-    var data={
-      "id":this.App.id_User
+    var data = {
+      "id": this.App.id_User
     }
-    await $.post("http://127.0.0.1:8000/api/notificaciones_participante/",data).done(function (notificacion) {
-    if(!notificacion.error){
-          self.listNotificaciones=notificacion.notificaciones
+    await $.post("http://127.0.0.1:8000/api/notificaciones_participante/", data).done(function (notificacion) {
+      if (!notificacion.error) {
+        self.listNotificaciones = notificacion.notificaciones
 
       }
-      else{
+      else {
         self.alertNotificacion(notificacion.mensaje)
       }
 
     })
-    
+
   }
 
-  async alertNotificacion(mensaje:string) {
-      const alert = await this.alertController.create({
-        header: 'Mensaje',
-        subHeader: 'Sin Notificaciones',
-        message: mensaje,
-        buttons: ['OK']
-      });
-  
-      await alert.present();
-    }
+  async alertNotificacion(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Mensaje',
+      subHeader: 'Sin Notificaciones',
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
